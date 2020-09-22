38bab102b9dfa8c57fc0a8a1bc0430b1917d5752
zqm64 <zqm64@outlook.com>
2020-09-22 14:28:06 +0800

    msm-poweroff: Re-Enable EDL Mode
    * Reverted xiaomi changes
    * Reverted commit 3f413d0c835671cac5aa62a05ad65d83eefb0a43

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -60,12 +60,12 @@ static void scm_disable_sdi(void);
 * There is no API from TZ to re-enable the registers.
 * So the SDI cannot be re-enabled when it already by-passed.
 */
-int download_mode = 0;
+int download_mode = 1;
 #else
-static const int download_mode;
+const int download_mode;
 #endif
 
-int in_panic = 0;
+int in_panic;
 
 static int panic_prep_restart(struct notifier_block *this,
 			      unsigned long event, void *ptr)
@@ -153,7 +153,6 @@ static bool get_dload_mode(void)
 	return dload_mode_enabled;
 }
 
-#if 0
 static void enable_emergency_dload_mode(void)
 {
 	int ret;
@@ -178,7 +177,6 @@ static void enable_emergency_dload_mode(void)
 	if (ret)
 		pr_err("Failed to set secure EDLOAD mode: %d\n", ret);
 }
-#endif
 
 static int dload_set(const char *val, struct kernel_param *kp)
 {
@@ -206,12 +204,10 @@ static void set_dload_mode(int on)
 	return;
 }
 
-#if 0
 static void enable_emergency_dload_mode(void)
 {
 	pr_err("dload mode is not enabled on target\n");
 }
-#endif
 
 static bool get_dload_mode(void)
 {
@@ -307,9 +303,7 @@ static void msm_restart_prepare(const char *cmd)
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 	}
 
-	if (in_panic) {
-		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
-	} else if (cmd != NULL) {
+	if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_BOOTLOADER);
@@ -341,13 +335,8 @@ static void msm_restart_prepare(const char *cmd)
 			if (!ret)
 				__raw_writel(0x6f656d00 | (code & 0xff),
 					     restart_reason);
-#if 0
 		} else if (!strncmp(cmd, "edl", 3)) {
-			if(0)
 			enable_emergency_dload_mode();
-			else
-			pr_info("This command already been disabled");
-#endif
 		} else {
 			__raw_writel(0x77665501, restart_reason);
 		}
